2c46270de56e3e0a0a6ce62f9ab24a52f781dc27
Mariusz Sidorowicz <marsidorowicz@gmail.com>
2020-03-31 20:24:11 +0200

    Zadania plus kilka nowych

--- Zadanie 2.py
+++ Zadanie 2.py
@@ -10,7 +10,7 @@ print(id(a), id(b), id(c))
 a=b=c=[1,2,3]
 print(a,b,c)
 print(id(a), id(b), id(c))
-a=[1,2,3,4]
+a.append(4)
 print(a,b,c)
 print(id(a), id(b), id(c)) # Nie zgadza się (identyfikator zmiennej a powinien być teraz taki sam jak b i c, dodatkowo zmieni się jednocześnie lista w zmiennych b i c)
 print()
---
+++ venv/Zadanie 3.py
@@ -0,0 +1,7 @@
+days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun']
+print(days)
+workdays = days.copy()
+print(workdays)
+workdays.remove('sat')
+workdays.remove('sun')
+print(workdays)
